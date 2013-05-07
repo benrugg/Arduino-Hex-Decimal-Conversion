63a2ca7ccee2d1aef8cf0cec5f1c4d9c81a62caa
Ben Rugg <ben@cloversites.com>
2013-05-06 20:44:09 -0700

    Adding the functions for hexToDec() and decToHex()

---
+++ hex_dec.ino
@@ -0,0 +1,44 @@
+
+// Converting from Hex to Decimal:
+
+// NOTE: This function can handle a positive hex value from 0 - 65,535 (a four digit hex string).
+//       For larger/longer values, change "unsigned int" to "long" in both places.
+
+
+unsigned int hexToDec(String hexString) {
+  
+  unsigned int decValue = 0;
+  int nextInt;
+  
+  for (int i = 0; i < hexString.length(); i++) {
+    
+    nextInt = int(hexString.charAt(i));
+    if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
+    if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
+    if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
+    nextInt = constrain(nextInt, 0, 15);
+    
+    decValue = (decValue * 16) + nextInt;
+  }
+  
+  return decValue;
+}
+
+
+
+
+
+// Converting from Decimal to Hex:
+
+// NOTE: This function can handle a positive decimal value from 0 - 255, and it will pad it
+//       with 0's (on the left) if it is less than the desired string length.
+//       For larger/longer values, change "byte" to "unsigned int" or "long" for the decValue parameter.
+
+
+String decToHex(byte decValue, byte desiredStringLength) {
+  
+  String hexString = String(decValue, HEX);
+  while (hexString.length() < desiredStringLength) hexString = "0" + hexString;
+  
+  return hexString;
+}
